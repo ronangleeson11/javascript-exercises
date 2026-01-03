433dae98f68327aece216d821d9fc7ecc01cafab
Ronan Gleeson <ronangleeson11@gmail.com>
2026-01-02 20:21:40 -0500

    Complete exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(op1, op2) {
+	return op1 + op2;
 };
 
-const subtract = function() {
-	
+const subtract = function(op1, op2) {
+	return op1 - op2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  	return arr.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(op1, op2) {
+	if (op2 === 0) {
+    return 1;
+  }
+  return op1 * power(op1, op2 - 1);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
