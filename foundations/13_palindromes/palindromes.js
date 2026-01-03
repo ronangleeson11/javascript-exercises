1b8144d133c95df3c3ca4b7d5c082e144463e618
Ronan Gleeson <ronangleeson11@gmail.com>
2026-01-02 20:39:14 -0500

    Complete exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let s = str.replace(/[^a-zA-Z0-9\s]/g, "").replaceAll(" ", "").toLowerCase();
+    console.log(s);
+    for (let i = 0; i < Math.floor(s.length / 2); i++) {
+        if (s.charAt(i) != s.charAt(s.length - i - 1)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
