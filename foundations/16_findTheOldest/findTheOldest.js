a61ece5b5e020cf441fa69dd5f4571a579f793df
Ronan Gleeson <ronangleeson11@gmail.com>
2026-01-02 23:40:32 -0500

    Finish foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let ret = arr
+                .map(person => {
+                    if (!person.yearOfDeath) {
+                        person.yearOfDeath = 2026;
+                    }
+                    if (!person.yearOfBirth) {
+                        person.yearOfBirth = person.yearOfDeath;
+                    }
+                    return {name: person.name, age: person.yearOfDeath - person.yearOfBirth};
+                })
+                .sort((a, b) => b.age - a.age); 
+    return ret[0];
 };
 
 // Do not edit below this line
